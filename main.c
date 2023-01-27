bb30140874efde21888b3137e987933e822e014d
pearlhulbert <pearl.k.hulbert@gmail.com>
2023-01-26 21:59:33 -0700

    code finished

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <openssl/sha.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
 
 u_int32_t shaWrapper(u_int64_t nonce, int bitSize);
-void collision(u_int64_t nonce, int bitSize);
+double calculateVariance(double*, double);
 
 int main(int argc, char **argv) {
     
@@ -14,28 +18,34 @@ int main(int argc, char **argv) {
         read(fileno(random), (unsigned char*)&initNonce, 8);
         // print our every number, then print out average, high and low
         // save each number of attempts 
-        u_int32_t target = shaWrapper(initNonce, bitSize);
-        u_int64_t newNonce = read(fileno(random), (unsigned char*)&newNonce, 8);
-        double counter = 0;
-        u_int32_t retVal = 0;
-        int min = 50;
-        int max = 0;
+        unsigned int counter = 0;
+        int match = 0;
+        u_int32_t currVal = 0;
+        double min = 50;
+        double max = 0;
         double attempts[50];
+        u_int32_t target = 0;
+        u_int64_t newNonce = read(fileno(random), (unsigned char*)&newNonce, 8);
         for (int i = 0; i < 50; ++i) {
-            while (retVal != target) {
-                retVal = shaWrapper(newNonce + counter, bitSize);
-                if (counter > max) {
-                    max = counter;
+            double attemptNum = 0;
+            u_int32_t target = shaWrapper(initNonce + i, bitSize);
+            currVal = shaWrapper(newNonce + i, bitSize);
+            match = 0;
+            while (!match) {
+                currVal = shaWrapper(newNonce + counter, bitSize);
+                if (target == currVal) {
+                    printf("Found match on attempt: %f\n", attemptNum);
+                    attempts[i] = attemptNum;
+                    match = 1;
                 }
-                else if (counter < min) {
-                    min = counter;
+                ++attemptNum;
+                if (attemptNum > max) {
+                    max = attemptNum;
                 }
-                ++counter;
-                attempts[i] = counter;
-                if (target == retVal) {
-                    printf("Found match on attempt: %d\n", counter);
-                    break;
+                else if (attemptNum < min) {
+                    min = attemptNum;
                 }
+                ++counter;  
             }
         }
         int numAttempts = 0;
@@ -43,44 +53,73 @@ int main(int argc, char **argv) {
             numAttempts += attempts[i];
         }
         double average = numAttempts / 50;
-        printf("average number of attempts: %d", average);
+        double variance = calculateVariance(attempts, average);
+        printf("average number of attempts: %f\n", average);
+        printf("min attempts: %f\n", min);
+        printf("max attempts: %f\n", max);
+        printf("variance: %f\n", variance);
     }
     else if (strcmp(argv[1], "-c") == 0) {
         int bitSize = atoi(argv[2]);
         int numPossible = 1 << bitSize;
-        u_int32_t totalHashes[numPossible];
         FILE* random = fopen("/dev/random", "r");
         u_int64_t nonce;
         read(fileno(random), (unsigned char*)&nonce, 8);
         // print our every number, then print out average, high and low
         // save each number of attempts 
         int collision = 0;
-        double counter = 0;
+        unsigned int counter = 0;
+        double min = 50;
+        double max = 0;
+        double attempts[50];
+        u_int32_t currVal = 0;
         for (int i = 0; i < 50; ++i) {
-            u_int32_t retVal = shaWrapper(nonce + counter, bitSize);
-            totalHashes[i] = retVal;
-            while (1) {
+            int total = 0;
+            currVal = shaWrapper(nonce + i, bitSize);
+            //printf("init currVal: %d\n", currVal);
+            u_int32_t totalHashes[numPossible];
+            totalHashes[total] = currVal;
+            double attemptNum = 0;
+            collision = 0;
+            bzero(totalHashes, numPossible);
+            while (!collision) {
+                ++counter;
+                currVal = shaWrapper(nonce + counter, bitSize);
+                //printf("currVal: %d\n", currVal);
+                ++attemptNum;
                 for (int j = 0; j < numPossible; ++j) {
-                    if (retVal == totalHashes[j]) {
-                        break;
+                    if (currVal == totalHashes[j]) {
+                        printf("collision on attempt: %f\n", attemptNum);
+                        if (attemptNum > max) {
+                            max = attemptNum;
+                        }
+                        else if (attemptNum < min) {
+                            min = attemptNum;
+                        }
+                        attempts[i] = attemptNum;
                         collision = 1;
+                        break;
                     }
                 }
-                if (!collision) {
-                    break;
-                }
+                totalHashes[total] = currVal;
+                ++total;
             }
-            ++counter;
         }
-        
-        
+        int numAttempts = 0;
+        for (int i = 0; i < 50; ++i) {
+            numAttempts += attempts[i];
+        }
+        double average = numAttempts / 50;
+        double variance = calculateVariance(attempts, average);
+        printf("average number of attempts: %f\n", average);
+        printf("min attempts: %f\n", min);
+        printf("max attempts: %f\n", max);
+        printf("variance: %f\n", variance);
     }
-
     return 0;
 }
 
 u_int32_t shaWrapper(u_int64_t nonce, int bitSize) {
-    unsigned char ibuf[] = "compute sha1";
     unsigned char obuf[20];
 
     SHA1((char*)&nonce, bitSize, obuf);
@@ -93,12 +132,27 @@ u_int32_t shaWrapper(u_int64_t nonce, int bitSize) {
     int mask = (1 << bitSize) - 1;
     forMask = forMask & mask;
 
-    // int i;
-    // for (i = 16; i < 20; i++) {
-    //     printf("%02x ", obuf[i]);
-    // }
-    // printf("\n");
-
     return forMask;
+}
+
+double calculateVariance(double* attempts, double average) {
+
+    double difference[50];
+    for (int i = 0; i < 50; ++i) {
+        difference[i] = attempts[i] - average;
+    }
+    
+    double squares[50];
+    for (int i = 0; i < 50; ++i) {
+        squares[i] = difference[i] * difference[i];
+    }
+
+    double sumOfSquares = 0;
+    for (int i = 0; i < 50; ++i) {
+        sumOfSquares += squares[i];
+    }
+
+    return sumOfSquares / 50;
+
 }
 
